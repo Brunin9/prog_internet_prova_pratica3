4404e3db870567ef5a878df896bc9060369222e7
Brunin9 <brubor2008@gmail.com>
2025-09-22 11:04:20 -0300

    em andamento

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import { router } from "./router/router.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api/dados", router);
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
